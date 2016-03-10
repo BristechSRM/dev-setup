78599ff5f61667cc949a9ed20a7f23acdfeeac20
David Wybourn <dwybourn@scottlogic.com>
2016-03-10 07:24:00 -0800

    Added gopath environment variable to .zshrc file

--- vagrantScripts/backend.sh
+++ vagrantScripts/backend.sh
@@ -29,7 +29,10 @@ sudo apt-get install -y docker-engine
 #
 sudo apt-get install -y golang
 
-echo '' >> /home/vagrant/.bashrc
-echo '#GO' >> /home/vagrant/.bashrc
-echo 'export GOPATH=/workspace/speaker-comms' >> /home/vagrant/.bashrc
-echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> /home/vagrant/.bashrc
+#
+# Add go environment variables for bash and zsh shells
+#
+echo '' | tee -a /home/vagrant/.bashrc /home/vagrant/.zshrc
+echo '#GO' | tee -a /home/vagrant/.bashrc /home/vagrant/.zshrc
+echo 'export GOPATH=/workspace/speaker-comms' | tee -a /home/vagrant/.bashrc /home/vagrant/.zshrc
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' | tee -a /home/vagrant/.bashrc /home/vagrant/.zshrc
