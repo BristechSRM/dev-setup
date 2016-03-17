cfd97e693e2e246cc5eeb8b44dd674f21c423267
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-03-17 11:32:44 +0000

    Reorder so that keyboard map is always run in case of aws cancel or script exit Add explanation for having keyboard mapping setup here instead of in vagrant up

--- firstUpConfig.sh
+++ firstUpConfig.sh
@@ -1,9 +1,13 @@
 echo "Beginning first up configuration"
+
+echo "Changing keyboard mapping to GB"
+# Requires x server which isn't running during vagrant setup 
+setxkbmap gb
+
 if command -v aws >/dev/null 2>&1; then
     echo "AWS CLI found. Starting interactive config"
     aws configure
 fi
 
-echo "Changing keyboard mapping to GB"
-setxkbmap gb
+
 echo "First up Configuration complete"  
