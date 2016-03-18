f78d9ad52515bd52b00a01c8103e49e3ddb6c22e
David Wybourn <dwybourn@scottlogic.com>
2016-03-18 08:40:19 -0700

    Fixing mkdir wrong directory

--- mono-develop/run.sh
+++ mono-develop/run.sh
@@ -4,7 +4,7 @@ if [ ! "$(id -u)" != "0" ]; then
 else
     if [ ! -d ~/.mono-develop ]; then
         echo "Creating directory ~/.mono-develop to store mono-develop plugins and settings"
-        mkdir -p ~/.mono-develop/.local/share
+        mkdir -p ~/.mono-develop/.local/share/MonoDevelop-5.0
         mkdir -p ~/.mono-develop/.cache/MonoDevelop-5.0
     fi
 fi
@@ -12,6 +12,6 @@ fi
 docker run -ti --rm -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /workspace:/workspace \
-    -v ~/.mono-develop/.local/share.MonoDevelop-5.0:/home/developer/.local/share/MonoDevelop-5.0 \
+    -v ~/.mono-develop/.local/share/MonoDevelop-5.0:/home/developer/.local/share/MonoDevelop-5.0 \
     -v ~/.mono-develop/.cache/MonoDevelop-5.0:/home/developer/.cache/MonoDevelop-5.0 \
     -p 9000:9000  monodevelop 
